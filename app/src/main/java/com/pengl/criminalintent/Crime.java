f2d31d152daadbf61767b3cd8aa802bcd5c7360c
pengliang <pengliang@qiyi.com>
2015-11-19 19:18:38 +0800

    Add first EditText fragment for Crime Title
    Signed-off-by: pengliang <pengliang@qiyi.com>

---
+++ app/src/main/java/com/pengl/criminalintent/Crime.java
@@ -0,0 +1,27 @@
+package com.pengl.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by pengl on 2015/11/18.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+}
---
+++ app/src/main/java/com/pengl/criminalintent/CrimeFragment.java
@@ -0,0 +1,49 @@
+package com.pengl.criminalintent;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+/**
+ * Created by pengl on 2015/11/18.
+ */
+public class CrimeFragment extends android.support.v4.app.Fragment {
+    private Crime mCrime;
+    private EditText mTitleField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    // API to Create fragment view's layout
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        // find resource by view.findViewById()
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        return v;
+    }
+}
