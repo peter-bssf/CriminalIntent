ee25cb42b6254419055fdda559b94c36f9149a2d
pengliang <pengliang@qiyi.com>
2015-11-20 20:34:40 +0800

    Efficient RecycleView Reloading
    Signed-off-by: pengliang <pengliang@qiyi.com>

--- app/src/main/java/com/pengl/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/pengl/criminalintent/CrimeListFragment.java
@@ -2,6 +2,7 @@ package com.pengl.criminalintent;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,9 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 /**
  * Created by pengl on 15-11-20.
@@ -20,6 +22,7 @@ import java.util.List;
 public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecycleView;
     private CrimeAdapter mAdapter;
+    private List<Integer> mPositionChanged = new ArrayList<Integer>();
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -33,6 +36,17 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
+    /*
+    private void iterateAllPosition() {
+        for(int i=0, size = mPositionChanged.size(); i<size; i++) {
+            int key = mPositionChanged.keyAt(i);
+            boolean changed = mPositionChanged.get(key);
+            if(changed) {
+                mAdapter.notifyItemChanged(key);
+            }
+        }
+    }*/
+
     private void updateUI() {
         CrimeLab crimeLab =CrimeLab.get(getActivity());
         List<Crime> crimes =crimeLab.getCrimes();
@@ -41,7 +55,10 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecycleView.setAdapter(mAdapter);
         } else {
-            mAdapter.notifyDataSetChanged();
+            //iterateAllPosition();
+            for(int i: mPositionChanged) {
+                mAdapter.notifyItemChanged(i);
+            }
         }
     }
 
@@ -65,11 +82,16 @@ public class CrimeListFragment extends Fragment {
             startActivity(i);
         }
 
-        public void bindCrime (Crime crime) {
+        public void bindCrime (Crime crime, int position) {
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
             mSolveCheckBox.setChecked(mCrime.isResolved());
+            for(int i: mPositionChanged) {
+                if(i == position)
+                    return;
+            }
+            mPositionChanged.add(position);
         }
     }
 
@@ -94,7 +116,7 @@ public class CrimeListFragment extends Fragment {
         @Override
         public void onBindViewHolder(CrimeHolder holder, int position) {
             Crime crime = mCrimes.get(position);
-            holder.bindCrime(crime);
+            holder.bindCrime(crime, position);
         }
     }
 
